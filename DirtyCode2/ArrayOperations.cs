1a3ba053e4b4165b10f36236c2c5d57e6ca94865
Jafar <jaafaroday22@gmail.com>
2023-10-23 21:05:41 +0300

    Add project files.

---
+++ DirtyCode2/ArrayOperations.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DirtyCode2
+{
+    public class ArrayOperations<T> where T : struct, IComparable
+    {
+
+
+        public T[] MyArray { get; set; }
+
+
+        public ArrayOperations(int size)
+        {
+            MyArray = new T[size];
+        }
+        public static T GetInput()
+        {
+
+            string input = Console.ReadLine();
+            return (T)Convert.ChangeType(input, typeof(T));
+        }
+        public void Populate()
+        {
+
+            Console.WriteLine($"Enter {MyArray.Length} numbers for array :");
+            for (int i = 0; i < MyArray.Length; i++)
+            {
+                MyArray[i] = GetInput();
+            }
+
+
+        }
+
+        public static double[] SumTwoArrays(T[] arr1, T[] arr2)
+        {
+
+
+            var resultArr = new double[arr1.Length];
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                resultArr[i] = Convert.ToDouble(arr1[i]) + Convert.ToDouble(arr2[i]);
+            }
+            Console.WriteLine("The sum of arrays is:");
+
+            return resultArr;
+        }
+
+
+        public void Average()
+        {
+
+
+            if (MyArray is int[] or double[] or decimal[] or float[])
+            {
+                double sum = 0;
+                for (int i = 0; i < MyArray.Length; i++)
+                {
+                    sum += Convert.ToDouble(MyArray[i]);
+                }
+                double average = sum / MyArray.Length;
+
+                Console.WriteLine("The average of array  is: " + average);
+
+            }
+            else
+            {
+
+                Console.WriteLine("Cant Calculate Average for this type");
+            }
+
+
+        }
+
+        public void Print()
+        {
+            for (int i = 0; i < MyArray.Length; i++)
+            {
+                Console.Write(MyArray[i] + " ");
+            }
+
+            Console.WriteLine();
+        }
+
+
+        public void Max()
+        {
+            T max = MyArray[0];
+            for (int i = 1; i < MyArray.Length; i++)
+            {
+                if (MyArray[i].CompareTo(max) > 0)
+                {
+                    max = MyArray[i];
+                }
+            }
+
+            Console.WriteLine("The maximum value in array  is: " + max);
+        }
+
+
+    }
+}
---
+++ DirtyCode2/Program.cs
@@ -0,0 +1,24 @@
+﻿using DirtyCode2;
+
+var arr1 = new ArrayOperations<double>(5)
+{
+    MyArray = new[] { 1.0, 2, 3, 4, 5 }
+};
+arr1.Print();
+arr1.Max();
+arr1.Average();
+ArrayOperations<double> arr2 = new(5)
+{
+    MyArray = new[] { 1.0, 2, 3, 4, 5 }
+};
+arr2.Print();
+arr2.Max();
+arr2.Average();
+
+var result = ArrayOperations<double>.SumTwoArrays(arr1.MyArray, arr2.MyArray);
+
+ArrayOperations<double> arr3 = new(5);
+arr3.MyArray = result;
+arr3.Print();
+arr3.Max();
+arr3.Average();
